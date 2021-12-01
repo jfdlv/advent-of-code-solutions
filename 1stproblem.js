7d9dd5c193749d8a86831e3727f54b4ba52b1121
Fernando De La Via <fernando.delavia@coxautoinc.com>
2021-12-01 11:44:28 -0400

    adds 1st problem solution

---
+++ 1stproblem.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+fs.readFile('./input.txt', (err, data)=> {
+  if(err) {
+    console.error(err);
+    return;
+  }
+  let arrayOfNumbers = data.toString().split('\r\n');
+  let count = 0;
+  arrayOfNumbers.forEach((element, index)=>{
+    let previousElement = arrayOfNumbers[index-1];
+    if(previousElement && parseInt(element) > parseInt(previousElement)) {
+      count++;
+    }
+  });
+  console.log('First Solution')
+  console.log(count);
+  let newArray = arrayOfNumbers.map((element, index)=>{
+    return parseInt(element) + parseInt(arrayOfNumbers[index + 1]) + parseInt(arrayOfNumbers[index +2]);
+  });
+  console.log(JSON.stringify(newArray));
+  let secondCount = 0;
+  newArray.forEach((element, index)=>{
+    let previousElement = newArray[index-1];
+    if(previousElement && parseInt(element) > parseInt(previousElement)) {
+      secondCount++;
+    }
+  });
+  console.log('Second Solution')
+  console.log(secondCount);
+})
